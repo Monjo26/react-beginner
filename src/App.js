54204fb4cdc7a3decc405b599eeede65e4705b92
Shweta Manjrekar <shweta.others@gmail.com>
2024-05-17 22:01:14 +0530

    Event binding with this keyword

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick/>
-      <ClassClick/>
+      {/* <FunctionClick/>
+      <ClassClick/> */
+      <EventBind/>
+      }
     </div>
   );
 }

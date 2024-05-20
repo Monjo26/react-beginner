b8c7b04f9dbfb80a55465e8ea1bd60e3e60b4175
Shweta Manjrekar <shweta.others@gmail.com>
2024-05-20 21:31:28 +0530

    Context p3

--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const UserContext = React.createContext();
+const UserContext = React.createContext("Shweta");
 
 const UserProvider = UserContext.Provider;
 const UserConsumer = UserContext.Consumer;
 
 export { UserConsumer, UserProvider };
+export default UserContext;

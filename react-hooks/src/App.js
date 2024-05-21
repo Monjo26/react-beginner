7fadcf7c74da0b01a77ca581acd098cb41737875
Shweta Manjrekar <shweta.others@gmail.com>
2024-05-21 22:15:57 +0530

    hooks -->useState Object

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
+import HookCounterThree from "./components/HookCounterThree";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <ClassCounter />
       <HookCounter /> */}
 
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
     </div>
   );
 }

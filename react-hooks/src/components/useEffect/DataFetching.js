b39aaaeea0a141445ca88e1e602a93c42105edca
Shweta Manjrekar <shweta.others@gmail.com>
2024-05-23 01:59:22 +0530

    fetching data with useEffect part 3

--- react-hooks/src/components/useEffect/DataFetching.js
+++ react-hooks/src/components/useEffect/DataFetching.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataFetching() {
-  const [post, setPost] = useState(null);
+  const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((response) => {
         console.log(response);
         setPost(response.data);
@@ -15,11 +16,16 @@ function DataFetching() {
       .catch((error) => {
         console.log(error);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
+
+  const handlClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={handlClick}>Change</button>
       <ul>${post.title}</ul>
     </div>
   );
